f0f27ee5f593efc55020df19f4f2302fc89b9aa5
Jordan M <jormacmoo@gmail.com>
2019-12-09 13:30:43 -0500

    create utility functions script

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,8 @@
+#Utility functions
+#Includes:
+#  reroll: Simulates a number of dice rolls between 1 and 5
+#  find_score: Create score for dice roll after it is checked for necessary parts
+
 reroll <- function(num_dice){
   if (is.numeric(num_dice) == FALSE && is.integer(num_dice) == FALSE) {
     stop("Bad input. Input must be a number between 1 and 5.")
@@ -9,5 +14,16 @@ reroll <- function(num_dice){
     message(cat("You've rolled a ", roll))
   } else {
     message(cat("You've rolled ", roll))
+  }
+}
+
+find_score <- function(needed, cargo) {
+  perfect <- c(6, 5, 4)
+  if (is_empty(needed)) {
+    return(0)
+  } else if (length(needed) < 3) {
+    return(0)
+  } else if (identical(needed, perfect)) {
+    return(sum(cargo))
   }
 }
