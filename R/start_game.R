7f59274154353ef5a931f0e390d146f232b60b5a
Jordan M <jormacmoo@gmail.com>
2019-12-09 19:17:35 -0500

    add functions

--- R/start_game.R
+++ R/start_game.R
@@ -4,4 +4,13 @@ start_game <- function(...) {
   # the first roll will always have five dice
   roll1 <- reroll(5)
   list[good_dice, leftover_dice] <- check_roll(roll1)
+  score <- find_score(good_dice, leftover_dice)
+  num_rolls <- 1
+
+  # messages
+
+
+  # give player option to reroll
+  reroll_prompt(length(leftover_dice))
+
 }
