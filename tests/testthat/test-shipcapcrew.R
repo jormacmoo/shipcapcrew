75fce9f3eeb47ec2bbc5cee41a71a2633112be1b
Paige PAtrick <ppatrick@smith.edu>
2019-12-18 22:56:16 -0500

    first half of vignette and R script for tests

---
+++ tests/testthat/test-shipcapcrew.R
@@ -0,0 +1,16 @@
+start_simulation(5)
+
+test_gamecard <- gamecard
+
+test_that("simulation works", {
+  expect_equal(ncol(start_simulation(3)), 3)
+  expect_is(start_simulation(4), )
+  #expect_error(start_simulation(p), "object 'p' not found")
+
+})
+
+# test_that("interactive works", {
+#
+#   expect_type(start_game(), "You've rolled")
+#
+# })
