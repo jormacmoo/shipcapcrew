2f49099c56773570e90aa33fa0169df6031fd2ce
Paige PAtrick <ppatrick@smith.edu>
2019-12-19 21:38:37 -0600

    please accept our tests, Travis

--- tests/testthat/test-shipcapcrew.R
+++ tests/testthat/test-shipcapcrew.R
@@ -1,16 +1,17 @@
-# start_simulation(5)
-#
-# test_gamecard <- gamecard
-#
-# test_that("simulation works", {
-#   expect_equal(ncol(start_simulation(3)), 3)
-#   expect_is(start_simulation(4), )
-  #expect_error(start_simulation(p), "object 'p' not found")
+test_that("interactive works", {
 
-# })
+  expect_type(game_turn(0, 5, NULL), "list")
+  expect_type(game_turn(1, 2, c(6, 5, 4)), "list")
+  expect_output(game_turn(2, 5, NULL))
 
-# test_that("interactive works", {
-#
-#   expect_lengh(start_game(), ".")
-#
-# })
+})
+
+test_that("simulation works", {
+
+  expect_error(start_simulation(p), "object 'p' not found")
+  expect_error(multiplayer_simulation("extra"), " integer or numeric argument of")
+  expect_error(start_simulation("hello"), "num_games should be an integer or numeric argument of")
+  expect_output(reset_simulation())
+
+})
+
