02126e0fb1e252d572aecab89eccc0b62bd0318a
Paige PAtrick <ppatrick@smith.edu>
2019-12-19 22:54:04 -0600

    re organizing and documenting tests

--- tests/testthat/test-interactive.R
+++ tests/testthat/test-interactive.R
@@ -1,8 +1,19 @@
 test_that("simulation works", {
 
+  # checks for correct error message when user enters an object for start_simulation() function
   expect_error(start_simulation(p), "object 'p' not found")
+
+  # checks for correct error message when user enters an string for multiplayer_simulation() function
   expect_error(multiplayer_simulation("extra"), " integer or numeric argument of")
+
+  # checks for correct error message when user enters an string for start_simulation() function
   expect_error(start_simulation("hello"), "num_games should be an integer or numeric argument of")
+
+  # checks if reset_simulation() code print output to the console
   expect_output(reset_simulation())
 
 })
+
+## As a group, we found it difficult to engage with test_that() for the simulation aspect of our package
+## As discussed in the shipcapcrew-vignette, start_simulation() and multi_player_simulation() don't explicitly retun anything
+## Because of this, we were unable to run very many checks that were not focused on error message
--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -1,9 +1,22 @@
 test_that("interactive works", {
 
-  #this reads
+  # this checks to see if the game_turn() returns a list,
+  # when previous rolls = 0, number of dice = 5, and
+  # previous dice from past rolls is NULL, because it doesn't apply since previous rolls = 0
   expect_type(game_turn(0, 5, NULL), "list")
+
+  # similar to last check, this checks to see if  game_turn() returns a list,
+  # this time though there was one previous roll, and there was only two dice remaining,
+  # since a 6, 5, and 4 were rolled on the previous roll.
   expect_type(game_turn(1, 2, c(6, 5, 4)), "list")
+
+  # checks if reset_simulation() code print output to the console
   expect_output(game_turn(2, 5, NULL))
+
+  # meant to check that the length of the list game_turn returns is 5
+  # wasn't able to get it figured out before 12:00am
+  #expect_length(list(game_turn(2, 4 c(6))), 5)
 
 })
 
+## We found it difficult to engage with start_game() function, for reasons similarly to those mentioned in the test-interactive.R
